0794f0009b3eecf609b8e0f83ab27ddcd946d7b5
Rodrigo Coelho <rodrigo.coelho@unifei.edu.br>
2020-05-01 17:17:42 -0300

    Add coisas novas no mapa e mod no personagem

--- servidor/componentes/jogo/mapa.py
+++ servidor/componentes/jogo/mapa.py
@@ -5,7 +5,28 @@ Created on 28 de abr de 2020
 '''
 
 class Mapa():
-    
-    def criar_mapa(self):
-        pass
-    
+    TAM = 50
+
+    def __init__(self):
+        tiles[TAM][TAM]
+
+        for i in range(TAM):
+            for j in range(TAM):
+                tiles[i][j]=0
+                #Insatanciando as bordas do mapa
+                if(i==0 or j ==0 or i==TAM-1 or j == TAM-1):
+                    tiles[i][j]=Pedra()
+
+        tiles[10][10]=Arbusto()
+        tiles[20][10]=Arbusto()
+        tiles[20][20]=Arbusto()
+        tiles[10][30]=Arbusto()
+        tiles[30][20]=Arbusto()
+        tiles[40][10]=Arbusto()
+        tiles[10][40]=Arbusto()
+
+    def verfica(self, X, Y):
+        if(tiles[X][Y]):
+            return false
+        else:
+            return true
--- servidor/componentes/jogo/personagem.py
+++ servidor/componentes/jogo/personagem.py
@@ -11,19 +11,18 @@ from componentes.jogo.thread_update import ThreadUpdate
 
 import math
 
-#Constante de tempo. 
-TIMER = 1.0
-
 class Personagem(ObjetosDinamicos):
-    
-    def __init__(self, id, posicao_x, posicao_y, 
+    #Constante de tempo.
+    TIMER = 1.0
+
+    def __init__(self, id, posicao_x, posicao_y,
                  direcao_x = 0, direcao_y = 0,
                  raio_bomba = 1, count_bomba = 1,
                  distancia_bomba = 1, angulo_bomba = 0):
-        
+
         #Chama o construtor da classe mãe
         super().__init__(posicao_x, posicao_y)
-        
+
         self.id = id
         self.direcao_x = direcao_x
         self.direcao_y = direcao_y
@@ -31,21 +30,20 @@ class Personagem(ObjetosDinamicos):
         self.count_bomba = count_bomba
         self.distancia_bomba = distancia_bomba
         self.angulo_bomba = angulo_bomba
-        
+
     def criar_bomba(self, x, y):
         #Calcula a posição final da bomba
         self.angulo_bomba = math.atan(y,x)
         posicao_final_x = self.raio_bomba*math.cos(self.angulo_bomba)
         posicao_final_y = self.raio_bomba*math.sin(self.angulo_bomba)
-        
+
         bomba = Bomba(TIMER, self.raio_bomba, posicao_final_x, posicao_final_y, self.dir_x, self.dir_y)
         ThreadUpdate.bombas.update({(x,y):bomba})
-    
+
     def andar(self, x, y):
         self.direcao_x = x
         self.direcao_y = y
-    
+
     def parar_andar(self):
         self.direcao_x = 0
         self.direcao_y = 0
-        
