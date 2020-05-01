1a537cdcadee390aad488228a85b27b23318bbf7
Leonardo Melo <leonardohenriquedemelo@outlook.com>
2020-05-01 18:47:07 -0300

    Modificação na classe arbusto.

--- servidor/componentes/jogo/arbusto.py
+++ servidor/componentes/jogo/arbusto.py
@@ -7,17 +7,39 @@ Content: Classe Arbusto
 '''
 from componentes.jogo.objetos_estaticos import ObjetosEstaticos
 import random
+from componentes.jogo.distancia_bomba import DistanciaBomba
+from componentes.jogo.multi_bomba import MultiBomba
+from componentes.jogo.raio_bomba import RaioBomba
+from componentes.jogo.velocidade import Velocidade
 
 class Arbusto(ObjetosEstaticos):
+    
+    def destruir(self, id):
+        #Seleciona um poder
+        objeto = self.is_power()
+        
+        #Aplica o poder
+        objeto.power(id)
+        
+        #Apaga o objeto que foi usado para fazer o poder
+        del objeto
 
+#Estas dudas funções podem virar uma só, mas precisamos passar o id para onde o poder vai...
+       
     def is_power(self):
-        r=random.randint(0,20)
-        switch(c):
-            case 1:#velocidade
-            case 2:#raiobomba
-            case 3:#distanciabomba
-            case 4:#multibomba
-            case default:
-
-# Acho mais facil implementar todos os PUps aqui direto já q são uma unica linha
-# Menos classes, tornariam o jogo mais simples . . . 
+        
+        r = random.randint(0,20)
+        
+        #Switch implementado com dicionário 
+        switcher = {
+            1: Velocidade(),
+            2: RaioBomba(),
+            3: DistanciaBomba(),
+            4: MultiBomba()
+        }
+        
+        #Escolher qual construtor será usado 
+        objeto = switcher.get(r, lambda: "Power Up Invalido.")
+        
+        return objeto
+        
--- servidor/componentes/jogo/bomba.py
+++ servidor/componentes/jogo/bomba.py
@@ -3,13 +3,14 @@ Created on 28 de abr de 2020
 
 @author: leonardo
 
-Content: Classe Bomba.
+Content: Classe Bomba. 
 '''
 from componentes.jogo.objetos_dinamicos import ObjetosDinamicos
 from componentes.jogo.thread_update import ThreadUpdate
+from componentes.servidor import Servidor
 
 class Bomba(ObjetosDinamicos):
-
+    
     def __init__(self, timer, raio_bomba, posicao_final_x, posicao_final_y, dir_x, dir_y):
         self.timer = timer
         self.raio_bomba = raio_bomba
@@ -17,21 +18,9 @@ class Bomba(ObjetosDinamicos):
         self.posicao_final_y = posicao_final_y
         self.dir_x = dir_x
         self.dir_y = dir_y
-
+        
     def bomba(self, posicao_final_x, posicao_final_y, distancia_bomba, raio_bomba, angulo):
         pass
-
+    
     def explodir(self):
-        ThreadUpdate.bombas.remove(self)
-        tilesX=self.posicao_final_x/20
-        tilesY=self.posicao_final_y/20
-        for i in range(tilesX):
-            for j in range(tilesY):
-                if(Mapa.tiles[tilesX][tilesY].destructible):
-                    # se puder ser distruido é um arbusto, portanto ver ser dropa um powerUp
-                for id in range(ThreadUpdade.personagens)
-                    if((ThreadUpdade.personagens[id].poricao_x == tilesX) and (ThreadUpdade.personagens[id].poricao_y == tilesY)):
-                        Servidor.gameOver(id)
-        #Explodir tudo que há no raio da bomba
-        #Se tem um personagem tem que dar gameOver
-        #se tem um arbusto tem que ver se ele vai gerar um PowerUp
+        Servidor.explode(self.posicao_final_x, self.posicao_final_y)
