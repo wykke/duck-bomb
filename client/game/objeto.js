751d9f0c8d8c0914ef0a2fa386bfb6732da964b2
Alex Junior Pereira <alexjp17@gmail.com>
2020-05-01 17:50:39 -0300

    objeto arrumado como no projeto

---
+++ client/game/objeto.js
@@ -0,0 +1,11 @@
+class Objeto{
+    constructor(){
+        this.posicaoX
+        this.posicaoY
+        this.tamanhoX
+        this.tamanhoY
+        this.layer
+        this.spriteIdle
+    }
+
+}
