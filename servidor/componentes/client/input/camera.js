0807c5828acd6ff961121caef87cc4359ac64b31
Alex Junior Pereira <alexjp17@gmail.com>
2020-05-20 10:40:36 -0300

    ajustado o smooth scroll do chrome

--- servidor/componentes/client/input/camera.js
+++ servidor/componentes/client/input/camera.js
@@ -1,8 +1,18 @@
 export default class Camera{
     constructor(playerDom){
         this.player = playerDom
+        this.isMoving = false
     }
     moveCamera(){
+        if(!this.isMoving){
+            this.isMoving = true
+            this.scroll()
+            setTimeout(()=>{
+                this.isMoving = false    
+            }, 100)
+        }
+    }
+    scroll(){
         this.player.scrollIntoView({ 
             behavior: 'smooth',
             block: 'center',
